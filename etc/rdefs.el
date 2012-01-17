0bf7529ff8d432cdcd6e3cbbc1cde86e023d6346
Ken Nishimura <ken97531@gmail.com>
2012-01-17 15:27:47 +0900

    didnt have to overwrite all the keybindings. my bad

--- etc/rdefs.el
+++ etc/rdefs.el
@@ -1,4 +1,10 @@
-;; some code to use rdefs from emacs
+;; some code for emacs
+;;
+;; add this to your dot.emacs
+;; (require 'rdefs)
+;; (add-hook 'ruby-mode-hook
+;;           (lambda ()
+;;             (define-key ruby-mode-map (kbd "M-e") 'ruby-rdefs)))
 
 (defconst rdefs-buffer-name "*rdefs*")
 (defconst rdefs-command "rdefs")
@@ -31,26 +37,5 @@
     (toggle-read-only 1)
     (ruby-mode)
     (rdefs-mode)))
-
-;;; ruby-mode key-bindings (extracted and modified from the original)
-;;; Visit a ruby source code file and press Meta(ESC)-e, and you get
-;;; the output from rdefs command.
-
-(setq ruby-mode-map (make-sparse-keymap))
-(define-key ruby-mode-map "\ee" 'ruby-defs)
-(define-key ruby-mode-map "{" 'ruby-electric-brace)
-(define-key ruby-mode-map "}" 'ruby-electric-brace)
-(define-key ruby-mode-map "\e\C-a" 'ruby-beginning-of-defun)
-(define-key ruby-mode-map "\e\C-e" 'ruby-end-of-defun)
-(define-key ruby-mode-map "\e\C-b" 'ruby-backward-sexp)
-(define-key ruby-mode-map "\e\C-f" 'ruby-forward-sexp)
-(define-key ruby-mode-map "\e\C-p" 'ruby-beginning-of-block)
-(define-key ruby-mode-map "\e\C-n" 'ruby-end-of-block)
-(define-key ruby-mode-map "\e\C-h" 'ruby-mark-defun)
-(define-key ruby-mode-map "\e\C-q" 'ruby-indent-exp)
-(define-key ruby-mode-map "\t" 'ruby-indent-command)
-(define-key ruby-mode-map "\C-c\C-e" 'ruby-insert-end)
-(define-key ruby-mode-map "\C-j" 'ruby-reindent-then-newline-and-indent)
-(define-key ruby-mode-map "\C-m" 'newline)
 
 (provide 'rdefs)
